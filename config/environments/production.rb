b4a2cf91516a1be4b59cc8b20b05511787706ef2
Domitrius Clark <domitrius.anthony@hotmail.com>
2017-03-27 05:40:45 -0400

    Added PG to gemfile for production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,5 +1,7 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
+  config.web_socket_server_url = "wss://doms-fakebook.herokuapp.com/cable" 
+  config.action_cable.allowed_request_origins = ['https://doms-fakebook.herokuapp.com', 'http://doms-fakebook.herokuapp.com']
 
   # Code is not reloaded between requests.
   config.cache_classes = true
