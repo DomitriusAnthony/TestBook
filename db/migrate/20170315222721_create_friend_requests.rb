ea1d60e6e57f50ae10cfb008abaff726f6150519
Domitrius Clark <domitrius.anthony@hotmail.com>
2017-03-27 16:15:49 -0400

    Changed a line in friend requests migration

--- db/migrate/20170315222721_create_friend_requests.rb
+++ db/migrate/20170315222721_create_friend_requests.rb
@@ -2,7 +2,7 @@ class CreateFriendRequests < ActiveRecord::Migration[5.0]
   def change
     create_table :friend_requests do |t|
       t.references :user, foreign_key: true
-      t.references :friend, foreign_key: true
+      t.references :friend, index: true
 
       t.timestamps
     end
